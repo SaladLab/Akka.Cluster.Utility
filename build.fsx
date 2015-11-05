276c44f6b0a42f99b8c22d9eee68e2f4fc165a3f
Esun Kim <veblush@gmail.com>
2015-11-05 15:02:58 +0900

    Use appveyor to make nightly build

--- build.fsx
+++ build.fsx
@@ -3,54 +3,175 @@
 
 open Fake
 open Fake.Testing.XUnit2
+open Fake.AssemblyInfoFile
 
-// Directories
-let outputDir = "output"
-let testDir = outputDir @@ "test"
-let nugetDir = outputDir @@ "nuget"
+// ------------------------------------------------------------------------------ Project
+
+let buildSolutionFile = "./Akka.Cluster.Utility.sln"
+let buildConfiguration = "Release"
+
+type Project = { 
+    Name: string;
+    Folder: string;
+    AssemblyVersion: string;
+    PackageVersion: string;
+    Dependencies: (string * string) list;
+}
+
+let decoratePrerelease v =
+    let couldParse, parsedInt = System.Int32.TryParse(v)
+    if couldParse then "build" + (sprintf "%04d" parsedInt) else v
+
+let decoratePackageVersion v =
+    if hasBuildParam "nugetprerelease" then
+        v + "-" + decoratePrerelease((getBuildParam "nugetprerelease"))
+    else
+        v
+
+let projects = ([
+    { 
+        Name="Akka.Cluster.Utility";
+        Folder="./core/Akka.Cluster.Utility";
+        AssemblyVersion="0.1.0";
+        PackageVersion="";
+        Dependencies=[("Akka", "1.0.4"); ("Akka.Cluster", "1.0.4.12-beta")];
+    }]
+    |> List.map (fun p -> { p with PackageVersion=decoratePackageVersion(p.AssemblyVersion) }))
+
+let project name =
+    List.filter (fun p -> p.Name = name) projects |> List.head
+
+// ---------------------------------------------------------------------------- Variables
+
+let binDir = "bin"
+let testDir = binDir @@ "test"
+let nugetDir = binDir @@ "nuget"
 let nugetWorkDir = nugetDir @@ "work"
 
-// Targets
+// ------------------------------------------------------------------------------ Targets
+
 Target "Clean" (fun _ -> 
-    CleanDirs [outputDir]
+    CleanDirs [binDir]
+    !! ("./core/**/bin/" + buildConfiguration + "/*.Tests.dll")
+    |> Seq.iter (fun p -> trace ("!" + p))
+)
+
+Target "AssemblyInfo" (fun _ ->
+    projects
+    |> List.iter (fun p -> 
+        CreateCSharpAssemblyInfo (p.Folder @@ "Properties" @@ "AssemblyInfoGenerated.cs")
+          [ Attribute.Version p.AssemblyVersion
+            Attribute.FileVersion p.AssemblyVersion ]
+        )
 )
 
 Target "Build" (fun _ ->
-    !!"./Akka.Cluster.Utility.sln"
-    |> MSBuildRelease "" "Rebuild"
+    !! buildSolutionFile
+    |> MSBuild "" "Rebuild" [ "Configuration", buildConfiguration ]
     |> Log "Build-Output: "
 )
 
 Target "Test" (fun _ ->  
     let xunitToolPath = findToolInSubPath "xunit.console.exe" "./packages/FAKE/xunit.runner.console*/tools"
     ensureDirectory testDir
-    !! ("./core/Akka.Cluster.Utility.Tests/bin/Release/Akka.Cluster.Utility.Tests.dll")
+    !! ("./core/**/bin/" + buildConfiguration + "/*.Tests.dll")
     |> xUnit2 (fun p -> 
         {p with 
             ToolPath = xunitToolPath;
             ShadowCopy = false;
-            XmlOutputPath = Some (testDir @@ "Akka.Cluster.Utility.Tests.xml") })
+            XmlOutputPath = Some (testDir @@ "TestResult.xml") })
 )
 
-Target "Nuget" (fun _ ->  
-    CopyFiles (nugetWorkDir @@ "lib/net45") ["./core/Akka.Cluster.Utility/bin/Release/Akka.Cluster.Utility.dll"]
+let createNugetPackages _ =
+    projects
+    |> List.iter (fun project -> 
+        let nugetFile = project.Folder @@ project.Name + ".nuspec";
+        let workDir = nugetWorkDir @@ project.Name;
 
-    NuGet (fun p -> 
-        {p with
-            Project = "Akka.Cluster.Utility"
-            OutputPath = nugetDir
-            WorkingDir = nugetWorkDir
-            Version = "0.1.0-dev20151104" }) 
-            "./core/Akka.Cluster.Utility/Akka.Cluster.Utility.nuspec"
+        let dllFileName = project.Folder @@ "bin/Release" @@ project.Name;
+        (!! (dllFileName + ".dll")
+         ++ (dllFileName + ".pdb")
+         ++ (dllFileName + ".xml")) |> CopyFiles (workDir @@ "lib/net45")
+
+        let isAssemblyInfo f = (filename f).Contains("AssemblyInfo")
+        let isSrc f = (hasExt ".cs" f) && not (isAssemblyInfo f)
+        CopyDir (workDir @@ @"src") project.Folder isSrc
+
+        NuGet (fun p -> 
+            {p with
+                Project = project.Name
+                OutputPath = nugetDir
+                WorkingDir = workDir
+                Dependencies = project.Dependencies
+                SymbolPackage = NugetSymbolPackage.Nuspec
+                Version = project.PackageVersion }) 
+                nugetFile
+    )
+
+let publishNugetPackages _ =
+    projects
+    |> List.iter (fun project -> 
+        NuGetPublish (fun p -> 
+            {p with
+                Project = project.Name
+                OutputPath = nugetDir
+                WorkingDir = nugetDir
+                AccessKey = getBuildParamOrDefault "nugetkey" ""
+                PublishUrl = getBuildParamOrDefault "nugetpublishurl" ""
+                Version = project.PackageVersion })
+
+        if hasBuildParam "nugetpublishurl" then (
+            // current FAKE doesn't support publishing symbol package with NuGetPublish.
+            // To workaround thid limitation, let's tweak Version to cheat nuget read symbol package
+            NuGetPublish (fun p -> 
+                {p with
+                    Project = project.Name
+                    OutputPath = nugetDir
+                    WorkingDir = nugetDir
+                    AccessKey = getBuildParamOrDefault "nugetkey" ""
+                    PublishUrl = getBuildParamOrDefault "nugetpublishurl" ""
+                    Version = project.PackageVersion + ".symbols" })
+        )
+    )
+
+Target "Nuget" <| fun _ ->
+    createNugetPackages()
+    publishNugetPackages()
+
+Target "CreateNuget" <| fun _ ->
+    createNugetPackages()
+
+Target "PublishNuget" <| fun _ ->
+    publishNugetPackages()
+
+Target "Help" (fun _ ->  
+    List.iter printfn [
+      "usage:"
+      "build [target]"
+      ""
+      " Targets for building:"
+      " * Build        Build"
+      " * Test         Build and Test"
+      " * Nuget        Create and publish nugets packages"
+      " * CreateNuget  Create nuget packages"
+      "                [ "
+      " * PublishNuget Publish nugets packages"
+      "                [nugetkey={API_KEY}] [nugetpublishurl={PUBLISH_URL}]"
+      ""]
 )
+
+// --------------------------------------------------------------------------- Dependency
 
 // Build order
 "Clean"
+  ==> "AssemblyInfo"
   ==> "Build"
   ==> "Test"
 
-// "Build"
-//  ==> "Nuget"
+"Build"
+  ==> "Nuget"
+
+"Build"
+  ==> "CreateNuget"
   
-// start build
-RunTargetOrDefault "Deploy"
+RunTargetOrDefault "Help"
